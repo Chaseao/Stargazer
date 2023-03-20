7c52ce30fbbf6455fce53246a833500139c4888e
Alex Contreras <57280113+alexsmart007@users.noreply.github.com>
2023-03-19 23:15:27 -0400

    Added in whitebox with the movement system

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,16 +7,11 @@ using UnityEngine.Windows;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private new Rigidbody rigidbody;
-    private Vector3 movementForce;
+    private new Rigidbody2D rigidbody;
+    private Vector2 movementForce;
     private Vector2 playerInput;
-    private float maxX;
-    private float maxZ;
     [SerializeField] private bool canMove = true;
-    [SerializeField] private float maxSpeed = 5f;
     [SerializeField] private float speed = 0.25f;
-    [SerializeField] private GameObject WickLeft;
-    [SerializeField] private GameObject WickRight;
     private Direction direction = Direction.Left;
 
     private enum Direction
@@ -27,22 +22,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        rigidbody = this.GetComponent<Rigidbody>();
-        maxX = maxSpeed;
-        maxZ = maxSpeed;
+        rigidbody = this.GetComponent<Rigidbody2D>();
+
     }
     private void Update()
     {
-        //Check for whether or not player is going higher than the max speed, added in another check for if the player wants to change directions
-       // if (Math.Abs(rigidbody.velocity.x) > maxX && (Mathf.Sign(movementForce.x) == Mathf.Sign(rigidbody.velocity.x))) movementForce.x = 0;
-       // if (Math.Abs(rigidbody.velocity.z) > maxZ && (Mathf.Sign(movementForce.z) == Mathf.Sign(rigidbody.velocity.z))) movementForce.z = 0;
-        //this.rigidbody.AddForce(movementForce, ForceMode.VelocityChange);
-        //Allows player to not slide
-        if(playerInput.Equals(Vector2.zero))
-        {
-            //rigidbody.velocity = Vector3.zero;
-        }
-
+        Physics2D.gravity = Vector2.zero;
         rigidbody.velocity = movementForce;
 
         switch (direction)
@@ -51,8 +36,6 @@ public class PlayerMovement : MonoBehaviour
                 if (playerInput.x > 0)
                 {
                     direction = Direction.Right;
-                    WickRight.SetActive(true);
-                    WickLeft.SetActive(false);
                 }
 
                 break;
@@ -60,8 +43,6 @@ public class PlayerMovement : MonoBehaviour
                 if (playerInput.x < 0)
                 {
                     direction = Direction.Left;
-                    WickLeft.SetActive(true);
-                    WickRight.SetActive(false);
                 }
 
                 break;
@@ -83,11 +64,11 @@ public class PlayerMovement : MonoBehaviour
     {
         if(!canMove)
         {
-            input = Vector3.zero;
+            input = Vector2.zero;
         }
         playerInput= input;
         input *= speed;
-        movementForce = new Vector3(input.x, 0f, input.y);
+        movementForce = new Vector2(input.x, input.y);
 
     }
 
