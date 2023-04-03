7f9f4b3afe8e281621cdd76ae283da5b1b0782ee
Chase <chaseao.oberg@gmail.com>
2023-04-03 14:19:01 -0400

    Water Updated

--- Assets/Scripts/Puzzle/Puzzles/WaterPuzzle/WaterHazards.cs
+++ Assets/Scripts/Puzzle/Puzzles/WaterPuzzle/WaterHazards.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaterHazards : MonoBehaviour
 {
     public event Action OnHazardClicked;
 
+    [SerializeField] float totalTime;
+    [SerializeField] Image flash;
+    [SerializeField] AnimationCurve flashPattern;
+
+    private void Awake()
+    {
+        flash.enabled = false;
+    }
+
     public void ClickHazard()
     {
         OnHazardClicked?.Invoke();
+        StartCoroutine(ActivateHazardBlink());
+    }
+
+    private IEnumerator ActivateHazardBlink()
+    {
+        float startTime = Time.time;
+        flash.enabled = true;
+
+        while(Time.time - startTime < totalTime)
+        {
+            Color temp = flash.color;
+            temp.a = flashPattern.Evaluate((Time.time - startTime) / totalTime);
+            flash.color = temp;
+            yield return null;
+        }
+
+        flash.enabled = false;
     }
 }
